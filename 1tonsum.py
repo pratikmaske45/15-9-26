b866f620538ff6184dcc3ba1cba81307905a81a3
Pratik Maske <pratik45maske@gmail.com>
2026-09-16 21:40:50 +0530

    updated programs

---
+++ 1tonsum.py
@@ -0,0 +1,9 @@
+i = 0
+sum = 0
+num = int(input("Enter the Number: "))
+while i <=num:
+     sum += i 
+     i += 1
+print ("the sum of numbers from 0 to" ,num, "is" ,sum)
+
+     
--- continue.py
+++ continue.py
@@ -1,7 +1,9 @@
-i = 0 
+i = 0
+
 while i < 10:
-    if i == 5: 
-     i += 1 
-    continue 
-print(i)
-i +=1 
+    if i == 5:
+        i += 1
+        continue
+
+    print(i)
+    i += 1
