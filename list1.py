d918c45c25f4a6ed970239b557887c985daf1ad4
Pratik Maske <pratik45maske@gmail.com>
2026-09-16 22:16:44 +0530

    new programs

---
+++ list1.py
@@ -0,0 +1,10 @@
+numbers =[10,20,30,40,50]
+total = 0
+count = 0
+i=0
+while i < 5:
+   total = total + numbers[i]
+   count =count + 1
+   i=i+1
+   average = total/count
+print("Average of given list is",average)
